df81427090935a6e016b3cd95b05f8de347e4de2
Maheshkumar P <maheshkumar.palanisamy@saama.com>
2024-12-02 18:26:41 +0530

    Add ruff and refactor

--- Day-01/part-2.py
+++ Day-01/part-2.py
@@ -1,4 +1,4 @@
-with open('input.txt', 'r') as f:
+with open("input.txt") as f:
     group_1 = []
     group_2 = []
     for line in f.readlines():
@@ -7,6 +7,7 @@ with open('input.txt', 'r') as f:
         group_2.append(int(b))
 
     from collections import Counter
+
     group_2_count = Counter(group_2)
 
     print(sum(i * group_2_count.get(i, 0) for i in group_1))
