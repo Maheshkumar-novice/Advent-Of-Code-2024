c2d507596d10f679572224cde5566dd757790f15
Maheshkumar P <callmeelprofessor@gmail.com>
2024-12-06 12:00:21 +0530

    Add benchmarks and optimize part 1

--- Day-04/part-2.py
+++ Day-04/part-2.py
@@ -27,3 +27,27 @@ for row in range(grid_length):
         if updates == 4 and values[0] != values[3] and values[1] != values[2]:
             count += 1
 print(count)
+
+
+"""
+# python 3.13
+ python3 -m cProfile part-2.py
+2046
+         159 function calls in 0.030 seconds
+
+   Ordered by: cumulative time
+
+   ncalls  tottime  percall  cumtime  percall filename:lineno(function)
+        1    0.000    0.000    0.030    0.030 {built-in method builtins.exec}
+        1    0.030    0.030    0.030    0.030 part-2.py:1(<module>)
+      140    0.000    0.000    0.000    0.000 {method 'strip' of 'str' objects}
+        1    0.000    0.000    0.000    0.000 {built-in method _io.open}
+        1    0.000    0.000    0.000    0.000 {built-in method builtins.print}
+        1    0.000    0.000    0.000    0.000 {method 'disable' of '_lsprof.Profiler' objects}
+        5    0.000    0.000    0.000    0.000 <frozen codecs>:322(decode)
+        1    0.000    0.000    0.000    0.000 {method '__exit__' of '_io._IOBase' objects}
+        5    0.000    0.000    0.000    0.000 {built-in method _codecs.utf_8_decode}
+        1    0.000    0.000    0.000    0.000 <frozen codecs>:312(__init__)
+        1    0.000    0.000    0.000    0.000 <frozen codecs>:263(__init__)
+        1    0.000    0.000    0.000    0.000 {built-in method builtins.len}
+"""
