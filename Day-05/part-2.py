f9e3a9f5910e081b77388b985fd37fa277fbb637
Maheshkumar P <callmeelprofessor@gmail.com>
2024-12-06 12:00:22 +0530

    Refactor day 5

--- Day-05/part-2.py
+++ Day-05/part-2.py
@@ -1,34 +1,57 @@
-with open("input.txt", "r") as f:
+with open("input.txt") as f:  # noqa: PTH123, INP001, D100
     rules, updates = f.read().split("\n\n")
-    rules = rules.split("\n")
 
     rules_dict = {}
+    for rule in rules.split("\n"):
+        before, after = rule.split("|")
 
-    for rule in rules:
-        before, after = list(map(int, rule.split("|")))
         if before not in rules_dict:
             rules_dict[before] = {"before": [], "after": []}
         if after not in rules_dict:
             rules_dict[after] = {"before": [], "after": []}
+
         rules_dict[before]["after"].append(after)
         rules_dict[after]["before"].append(before)
 
-    updates = [list(map(int, update.split(","))) for update in updates.split("\n")]
-
-    incorrect_updates = []
-    for update in updates:
+    result = 0
+    for update in updates.split("\n"):
+        update, incorrect = update.split(","), False  # noqa: PLW2901
         for idx, page in enumerate(update[1:]):
             if update[idx] not in rules_dict[page]["before"]:
-                incorrect_updates.append(update)
+                incorrect = True
                 break
 
-    updating = True
-    while updating:
-        updating = False
-        for update in incorrect_updates:
-            for idx, page in enumerate(update[1:]):
-                if update[idx] not in rules_dict[page]["before"]:
-                    updating = True
-                    update[idx], update[idx + 1] = update[idx + 1], update[idx]
+        if incorrect:
+            updating = True
+            while updating:
+                updating = False
+                for idx, page in enumerate(update[1:]):
+                    if update[idx] not in rules_dict[page]["before"]:
+                        updating = True
+                        update[idx], update[idx + 1] = update[idx + 1], update[idx]
+            result += int(update[len(update) // 2])
 
-    print(sum(update[len(update) // 2] for update in incorrect_updates))
+    print(result)  # noqa: T201
+
+"""
+5900
+         3841 function calls in 0.009 seconds
+
+   Ordered by: cumulative time
+
+   ncalls  tottime  percall  cumtime  percall filename:lineno(function)
+        1    0.000    0.000    0.009    0.009 {built-in method builtins.exec}
+        1    0.008    0.008    0.009    0.009 part-2.py:1(<module>)
+     1371    0.000    0.000    0.000    0.000 {method 'split' of 'str' objects}
+     2352    0.000    0.000    0.000    0.000 {method 'append' of 'list' objects}
+        1    0.000    0.000    0.000    0.000 {method 'read' of '_io.TextIOWrapper' objects}
+        1    0.000    0.000    0.000    0.000 {built-in method _io.open}
+        1    0.000    0.000    0.000    0.000 {built-in method builtins.print}
+        1    0.000    0.000    0.000    0.000 {method 'disable' of '_lsprof.Profiler' objects}
+      107    0.000    0.000    0.000    0.000 {built-in method builtins.len}
+        1    0.000    0.000    0.000    0.000 {method '__exit__' of '_io._IOBase' objects}
+        1    0.000    0.000    0.000    0.000 <frozen codecs>:319(decode)
+        1    0.000    0.000    0.000    0.000 <frozen codecs>:309(__init__)
+        1    0.000    0.000    0.000    0.000 {built-in method _codecs.utf_8_decode}
+        1    0.000    0.000    0.000    0.000 <frozen codecs>:260(__init__)
+"""
