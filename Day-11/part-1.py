5e6b604a4bab44cafe6731fafd89c7fd6b8b3101
Maheshkumar P <callmeelprofessor@gmail.com>
2024-12-11 16:32:06 +0530

    Add missing stones again

--- Day-11/part-1.py
+++ Day-11/part-1.py
@@ -1,4 +1,5 @@
 with open("input.txt") as f:  # noqa: D100, INP001, PTH123
+    stones = f.read().split()
 
     def _apply_rule(stone: str):  # noqa: ANN202
         if stone == "0":
@@ -10,5 +11,5 @@ with open("input.txt") as f:  # noqa: D100, INP001, PTH123
             yield str(int(stone) * 2024)
 
     for i in range(25):
-        stones = [i for stone in f.read().split() for i in _apply_rule(stone)]
+        stones = [i for stone in stones for i in _apply_rule(stone)]
     print(f"{len(stones):,}")  # noqa: T201
