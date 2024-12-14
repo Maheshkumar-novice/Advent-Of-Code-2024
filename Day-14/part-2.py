bac6d5f5e7108f5628eced1301aa7a8000f64703
Maheshkumar P <callmeelprofessor@gmail.com>
2024-12-14 17:19:59 +0530

    Add 2024:14

---
+++ Day-14/part-2.py
@@ -0,0 +1,32 @@
+import re
+import sys
+
+with open("input.txt") as f:
+    values = [re.findall(r"(-?\d+),(-?\d+)", line) for line in f]
+    grid = [["." for _ in range(101)] for _ in range(103)]
+    max_x, max_y = 103, 101
+    middle_x, middle_y = max_x // 2, max_y // 2
+    time_elapsed = 1
+
+    while True:
+        answers = set()
+        grid = [["." for _ in range(101)] for _ in range(103)]
+        for position, velocity in values:
+            vy, vx = int(velocity[0]) * time_elapsed, int(velocity[1]) * time_elapsed
+            py, px = int(position[0]), int(position[1])
+            px, py = (px + vx) % max_x, (py + vy) % max_y
+            grid[px][py] = "#"
+
+            answers.add((px, py))
+            if (px, py + 1) in answers:
+                flag = True
+                for i in range(2, 8):
+                    new_py = py + i
+                    if (px, new_py) in answers:
+                        continue
+                    flag = False
+                if flag:
+                    print(time_elapsed)
+                    sys.exit()
+
+        time_elapsed += 1
