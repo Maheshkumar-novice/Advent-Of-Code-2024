62f2f6ec8d5a106ed650e18a1f7b368f31fcc4f7
Maheshkumar P <67100964+Maheshkumar-novice@users.noreply.github.com>
2024-12-25 18:56:40 +0530

    Complete 2024:20

--- Day-20/part-1.py
+++ Day-20/part-1.py
@@ -1,92 +1,68 @@
 from collections import deque
-from copy import deepcopy
+
+
+def get_manhattan_points(x0, y0, distance):
+    # For each possible x-offset from -distance to +distance
+    for dx in range(-distance, distance + 1):
+        # The remaining distance must be covered by the y-coordinate
+        dy = distance - abs(dx)
+
+        # Add the point with positive dy
+        yield (x0 + dx, y0 + dy)
+
+        # Add the point with negative dy (if dy != 0)
+        if dy != 0:
+            yield (x0 + dx, y0 - dy)
+
 
 with open("input.txt") as f:
     grid = [list(line.strip()) for line in f]
-    hgrid = deepcopy(grid)
     grid_length = len(grid)
     directions = {"n": (-1, 0), "s": (1, 0), "e": (0, 1), "w": (0, -1)}
     sx, sy = next((i, j) for i in range(grid_length) for j in range(grid_length) if grid[i][j] == "S")
     ex, ey = next((i, j) for i in range(grid_length) for j in range(grid_length) if grid[i][j] == "E")
-    hashes = []
+    cheat_count = 0
+    possible_cheats = set()
 
     def _next_directions(x: int, y: int):  # noqa: ANN202
-        next_directions = []
         for dx, dy in directions.values():
             nx, ny = x + dx, y + dy
             if 0 <= nx < grid_length and 0 <= ny < grid_length:
                 if grid[nx][ny] != "#":
-                    next_directions.append((nx, ny))
+                    yield (nx, ny)
                 else:
-                    h = [(x, y)]
-                    h.append((nx, ny))
-                    nnx, nny = nx + dx, ny + dy
-                    if 0 <= nnx < grid_length and 0 <= nny < grid_length:
-                        h.append((nnx, nny))
-                    nnnx, nnny = nnx + dx, nny + dy
-                    if 0 <= nnnx < grid_length and 0 <= nnny < grid_length:
-                        h.append((nnnx, nnny))
-                    hashes.append(h)
-        return next_directions
+                    for i in range(1, 3):
+                        for px, py in get_manhattan_points(x, y, i):
+                            if 0 <= px < grid_length and 0 <= py < grid_length and grid[px][py] != "#":
+                                possible_cheats.add((px, py, x, y, i))
 
     queue = deque([(sx, sy, 0)])
     min_score = float("inf")
-    visited = list()
+    visited = set()
     while queue:
         x, y, score = queue.popleft()
         if (x, y) in visited:
             continue
-        # grid[x][y]= "O"
-        visited.append((x, y))
+        grid[x][y] = score
+        visited.add((x, y))
 
         for nx, ny in _next_directions(x, y):
             if (nx, ny) == (ex, ey):
                 min_score = min(score + 1, min_score)
             else:
                 queue.append((nx, ny, score + 1))
-        # import os
-        # from time import sleep
-        # print("\n".join(["".join(l) for l in grid]))
-        # sleep(0.1)
-        # os.system("clear")
+
+    grid[sx][sy] = 0
+    grid[ex][ey] = min_score
+
+    for cex, cey, csx, csy, dist in possible_cheats:
+        total = min_score
+        dist_to_end = min_score - grid[cex][cey]
+        dist_to_start = grid[csx][csy]
+
+        saved = (total - dist_to_end - dist_to_start) - dist
+        if saved >= 100:
+            cheat_count += 1
 
     print(min_score)
-    # exit()
-    # print("\n".join(["".join(l) for l in grid]))
-    visited.append((ex, ey))
-    # print(visited)
-    # print(hashes)
-    for a in hashes:
-        for x, y in a:
-            hgrid[x][y] = "&"
-    # print("\n".join(["".join(l) for l in hgrid]))
-
-
-    cd = 0
-    for hash in sorted(hashes):
-        if len(hash) == 2:
-            continue
-
-        if len(hash) == 3:
-            i, a, b = hash
-            if grid[b[0]][b[1]] == ".":
-                print(visited.index((b[0], b[1])))
-
-        if len(hash) == 4:
-            i, a, b, c = hash
-            # print(grid[a[0]][a[1]])
-            if grid[b[0]][b[1]] == ".":
-                k = visited.index((b[0], b[1]))
-                l = visited.index((i[0], i[1]))
-                if 9440 - (l + (9440 - k) + 2) >= 100:
-                    # print(84 - (l + (84 - k) + 2))
-                    cd += 1
-                # print(a,b,c, grid[a[0]][a[1]], grid[b[0]][b[1]],grid[c[0]][c[1]], l, k, l + (84 - k) + 2, 84 - (l + (84 - k) + 2))
-            elif grid[c[0]][c[1]] == ".":
-                k =  visited.index((c[0], c[1]))
-                l = visited.index((i[0], i[1]))
-                if 9440 - (l + (9440 - k) + 2) >= 100:
-                    # print(84 - (l + (84 - k) + 2))
-                    cd += 1
-                # print(a,b,c, grid[a[0]][a[1]], grid[b[0]][b[1]], grid[c[0]][c[1]], l, k, l + (84 - k) + 2, 84 - (l + (84 - k) + 2))
-    print(cd)
+    print(cheat_count)
--- Day-20/part-2.py
+++ Day-20/part-2.py
@@ -1,96 +1,68 @@
 from collections import deque
-from copy import deepcopy
 
-with open("sample.txt") as f:
+
+def get_manhattan_points(x0, y0, distance):
+    # For each possible x-offset from -distance to +distance
+    for dx in range(-distance, distance + 1):
+        # The remaining distance must be covered by the y-coordinate
+        dy = distance - abs(dx)
+
+        # Add the point with positive dy
+        yield (x0 + dx, y0 + dy)
+
+        # Add the point with negative dy (if dy != 0)
+        if dy != 0:
+            yield (x0 + dx, y0 - dy)
+
+
+with open("input.txt") as f:
     grid = [list(line.strip()) for line in f]
-    hgrid = deepcopy(grid)
     grid_length = len(grid)
     directions = {"n": (-1, 0), "s": (1, 0), "e": (0, 1), "w": (0, -1)}
     sx, sy = next((i, j) for i in range(grid_length) for j in range(grid_length) if grid[i][j] == "S")
     ex, ey = next((i, j) for i in range(grid_length) for j in range(grid_length) if grid[i][j] == "E")
-    hashes = []
+    cheat_count = 0
+    possible_cheats = set()
 
     def _next_directions(x: int, y: int):  # noqa: ANN202
-        next_directions = []
         for dx, dy in directions.values():
             nx, ny = x + dx, y + dy
             if 0 <= nx < grid_length and 0 <= ny < grid_length:
                 if grid[nx][ny] != "#":
-                    next_directions.append((nx, ny))
+                    yield (nx, ny)
                 else:
-                    h = [(x, y)]
-                    h.append((nx, ny))
-
-                    nnx, nny = nx, ny
-                    for _ in range(4):
-                        nnx, nny = nnx + dx, nny + dy
-                        if 0 <= nnx < grid_length and 0 <= nny < grid_length:
-                            h.append((nnx, nny))
-                    #     h.append((nnx, nny))
-                    # nnnx, nnny = nnx + dx, nny + dy
-                    # if 0 <= nnnx < grid_length and 0 <= nnny < grid_length:
-                    #     h.append((nnnx, nnny))
-                    hashes.append(h)
-        return next_directions
+                    for i in range(1, 21):
+                        for px, py in get_manhattan_points(x, y, i):
+                            if 0 <= px < grid_length and 0 <= py < grid_length and grid[px][py] != "#":
+                                possible_cheats.add((px, py, x, y, i))
 
     queue = deque([(sx, sy, 0)])
     min_score = float("inf")
-    visited = list()
+    visited = set()
     while queue:
         x, y, score = queue.popleft()
         if (x, y) in visited:
             continue
-        # grid[x][y]= "O"
-        visited.append((x, y))
+        grid[x][y] = score
+        visited.add((x, y))
 
         for nx, ny in _next_directions(x, y):
             if (nx, ny) == (ex, ey):
                 min_score = min(score + 1, min_score)
             else:
                 queue.append((nx, ny, score + 1))
-        # import os
-        # from time import sleep
-        # print("\n".join(["".join(l) for l in grid]))
-        # sleep(0.1)
-        # os.system("clear")
 
-    # print(min_score)
-    # print("\n".join(["".join(l) for l in grid]))
-    visited.append((ex, ey))
-    # print(visited)
-    print(hashes)
-    for a in hashes:
-        for x, y in a:
-            hgrid[x][y] = "&"
-    # print("\n".join(["".join(l) for l in hgrid]))
+    grid[sx][sy] = 0
+    grid[ex][ey] = min_score
 
-    cd = 0
-    vs = set(visited)
-    for hash in sorted(hashes):
-        print(hash)
-        l1, l2 = hash[-1]
-        if (l1, l2) not in vs:
-            continue
-        f1, f2 = hash[0]
-        if (f1, f2) not in vs:
-            continue
-        k = visited.index((hash[-1][0], hash[-1][1]))
-        l = visited.index((hash[0][0], hash[0][1]))
-        if 84 - (l + (84 - k) + 2) >= 50:
-            # print(84 - (l + (84 - k) + 2))
-            cd += 1
-        # for p, q in (hash):
-        #     if grid[p][q] == ".":
-        #         try:
-        #             k = visited.index((hash[-1][0], hash[-1][1]))
-        #             l = visited.index((hash[0][0], hash[0][1]))
-        #             print(p, q, k, l, hash[0][0], hash[0][1])
-        #             if 84 - (l + (84 - k) + 2) >= 50:
-        #                 # print(84 - (l + (84 - k) + 2))
-        #                 cd += 1
-        #         except Exception as e:
-        #             import traceback
-        #             traceback.print_exc()
-        #             break
+    for cex, cey, csx, csy, dist in possible_cheats:
+        total = min_score
+        dist_to_end = min_score - grid[cex][cey]
+        dist_to_start = grid[csx][csy]
 
-    print(cd)
+        saved = (total - dist_to_end - dist_to_start) - dist
+        if saved >= 100:
+            cheat_count += 1
+
+    print(min_score)
+    print(cheat_count)
